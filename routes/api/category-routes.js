48e243b4be1731ee25f0ee065ebee2c0f34ee31b
Sam Page <sampage6@gmail.com>
2021-12-30 00:22:26 -0600

    updating the findByPk request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,6 +18,18 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
