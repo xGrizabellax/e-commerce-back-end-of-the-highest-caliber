5b21d56d8bb4091d0226bea6f518429298143572
Sam Page <sampage6@gmail.com>
2021-12-30 01:55:07 -0600

    updating the put and post requests

--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 
 USE ecommerce_db;
-
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,12 +33,6 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  try {
-    const categoryData = await Product.create(req.body);
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
