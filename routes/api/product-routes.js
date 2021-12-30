ab4af257fb1aead58daffc33c3e2470c0a440c37
Sam Page <sampage6@gmail.com>
2021-12-30 01:05:59 -0600

    finished one get request for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,17 +6,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category}, {model: Tag}]
+    })
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json('Oops! Something went wrong!')
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, {model: Tag}]
+    })
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json('Oops! Something went wrong!')
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
 router.post('/', (req, res) => {
+
   /* req.body should look like this...
     {
       product_name: "Basketball",
