afffbfa2f6302852c5c4e1bbf1fe04c79e176d66
Sam Page <sampage6@gmail.com>
2021-12-30 19:28:01 -0600

    final updates

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,7 +48,7 @@ router.put('/:id', async (req, res) => {
       }
     });
     if (!tagData[0]) {
-      res.status(404).json({ message: 'No category with this id!' });
+      res.status(404).json({ message: 'No tag with this id!' });
       return;
     }
     res.status(200).json(tagData);
